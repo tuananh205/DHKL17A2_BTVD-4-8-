3fc510253e51c55046b9faec71225f866270fd0f
tuananh205 <ntanh.dhkl17a2hn@sv.uneti.edu.vn>
2023-11-09 20:06:43 +0700

    firts commit

---
+++ bt1_chuong4.py
@@ -0,0 +1,6 @@
+number = float(input("nhập một số: "))
+if number > 0:
+    square = number ** 2
+    print("Bình phương của", number, "là", square)
+else:
+    print("Số nhập vào không phải là số dương")
