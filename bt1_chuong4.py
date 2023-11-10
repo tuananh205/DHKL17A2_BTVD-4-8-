916281a7f32776616745f1f06aea03ff653fae63
tuananh205 <ntanh.dhkl17a2hn@sv.uneti.edu.vn>
2023-11-10 09:32:47 +0700

    first commit

--- bt1_chuong4.py
+++ bt1_chuong4.py
@@ -1,6 +1,5 @@
-number = float(input("nhập một số: "))
-if number > 0:
-    square = number ** 2
-    print("Bình phương của", number, "là", square)
-else:
-    print("Số nhập vào không phải là số dương")
+#nhập giá trị x
+x=float(input("nhập số x: "))
+if x > 0:
+     A= x**2
+     print(f"binh phương của {x} là {A}")
